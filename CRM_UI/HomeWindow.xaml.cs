f0956a1d57b569fe40992b6da418a0d1d27a0768
Vlalin <vlinskiy.ua@gmail.com>
2019-12-18 16:59:58 +0200

    В ПО CRM создано главное кно и кнопка Склад

---
+++ CRM_UI/HomeWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using CRM_UI.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CRM_UI
+{
+    /// <summary>
+    /// Логика взаимодействия для HomeWindow.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+        public HomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+
+
+            switch (index)
+            {
+                case 6:
+
+
+                    GridMain.Children.Clear();
+                    Navigation_Bar form = new Navigation_Bar();
+                    GridMain.Children.Add(form);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //private void ButtonPowerOff1_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    ButtonPowerOff1.Background = Brushes.Red;
+        //}
+
+        //private void ButtonPowerOff1_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    ButtonPowerOff1.Background = Brushes.White;
+        //    //////ButtonPowerOff1.Style.Setters["MaterialDesignFloatingActionMiniAccentButton"] = null;
+        //}
+    }
+}
---
+++ CRM_UI/Models/TodoModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CRM_UI.Models
+{
+    class TodoModel
+    {
+        private string _vendorcode;
+        public string vendorcode
+        {
+            get { return _vendorcode; }
+            set { _vendorcode = value; }
+        }
+        //public Image image (string filename);
+        private string _Title;
+        public string Title
+        {
+            get { return _Title; }
+            set { _Title = value; }
+        }
+        private string _Measurements;
+        public string Measurements
+        {
+            get { return _Measurements; }
+            set { _Measurements = value; }
+        }
+        private double _CostPrice;
+        public double CostPrice
+        {
+            get { return _CostPrice; }
+            set { _CostPrice = value; }
+        }
+        private double _Price;
+        public double Price
+        {
+            get { return _Price; }
+            set { _Price = value; }
+        }
+        private double _Weight;
+        public double Weight
+        {
+            get { return _Weight; }
+            set { _Weight = value; }
+        }
+        private double _Volume;
+        public double Volume
+        {
+            get { return _Volume; }
+            set { _Volume = value; }
+        }
+        private string _Url;
+        public string Url
+        {
+            get { return _Url; }
+            set { _Url = value; }
+        }
+        private bool _InStock;
+        public bool InStock
+        {
+            get { return _InStock; }
+            set { _InStock = value; }
+        }
+        private string _Total;
+        public string Total
+        {
+            get { return _Total; }
+            set { _Total = value; }
+        }
+        //public string vendorcode { get; set; }
+        private string _Irrelevant;
+        public string Irrelevant
+        {
+            get { return _Irrelevant; }
+            set { _Irrelevant = value; }
+        }
+
+        private string _Marketplace;
+        public string Marketplace 
+        {
+            get { return _Marketplace; }
+            set { _Marketplace = value; }
+        }
+
+        private bool _isDone;
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+    }
+}
